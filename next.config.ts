c5004fa4721e2e530d7caaba80335888a2527da7
Roki <rokiroydev@gmail.com>
2026-02-15 02:00:34 +0530

    refactor: remove `basePath` and `assetPrefix` from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/wowfy' : '',
-  assetPrefix: isProd ? '/wowfy/' : '',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
